ed62d57670fb6f58680db183ec55fdece5bd773a
Nakul Niroula <niroulanakul@gmail.com>
2021-12-25 01:55:59 +0545

    code cleaned

--- Box-Ball-Collision/Scripts/ball.js
+++ Box-Ball-Collision/Scripts/ball.js
@@ -12,25 +12,5 @@ function ball(x, y, r, color) {
     ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.fill();
-    
-  };
-
-  // this.newPos = function () {
-  //   if (
-  //     this.x + this.dx > animationArea.canvas.width - this.r ||
-  //     this.x + this.dx < this.r
-  //   ) {
-  //     this.dx = -this.dx;
-  //   }
-  //   if (
-  //     this.y + this.dy > animationArea.canvas.height - this.r ||
-  //     this.y + this.dy < this.r
-  //   ) {
-  //     this.dy = -this.dy;
-  //   }
-
-  //   this.x += this.dx;
-  //   this.y += this.dy;
-    
-  // };  
+  }; 
 }
