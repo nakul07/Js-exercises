4d8c75a9d2a5b5be13c7bf61eae852543096ef73
Nakul Niroula <niroulanakul@gmail.com>
2021-12-27 16:50:25 +0545

    Completed box ball

--- Box-Ball-Collision/Scripts/index.js
+++ Box-Ball-Collision/Scripts/index.js
@@ -6,7 +6,6 @@ const possilbeColors = [
   "blue",
   "black",
   "pink",
-  "white",
   "yellow",
   "purple",
   "green",
@@ -21,21 +20,23 @@ function startAnimation() {
 var animationArea = {
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 900;
+    this.canvas.width = 1000;
     this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateAnimationArea, 20);
+    this.interval = setInterval(updateAnimationArea, 16.66);
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
 };
+
 function updateAnimationArea() {
   animationArea.clear();
   finalBalls.forEach((ball) => {
-    ball.update();
-    collisionDetect(ball);
+    
     wallCollisionDetect(ball);
+    collisionDetect(ball);
+    ball.update();
   });
 }
