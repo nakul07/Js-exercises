335f8869abe3be8f57c5ee14176baed0e77bf3de
Nakul Niroula <niroulanakul@gmail.com>
2021-12-29 05:24:33 +0545

    completed

--- Car-Lane-Game/src/game.js
+++ Car-Lane-Game/src/game.js
@@ -1,24 +1,41 @@
-function obstacle(speed, x) {
+function obstacle(speed, y) {
   this.speed = speed;
-  this.x = x; //220;
-  this.y = -100;
+  this.index = RandomIndex();
+  this.x = obstaclePosition[this.index];
+  this.y = y;
+  this.yi = this.y;
   this.dy = 1;
+
   this.img = document.createElement("img");
   this.img.src = "images/obstacle.png";
+
   this.obstacleUpdate = function () {
     ctx = animationArea.context;
     ctx.drawImage(this.img, this.x, this.y, 60, 95);
+    this.moveObstacles();
+    if (this.y >= animationArea.canvas.height) {
+      this.reset();
+      score++;
+      this.speed = this.speed + 0.3;
+      // console.log(this.speed);
+    }
   };
+
   this.moveObstacles = function () {
     this.y += this.dy * this.speed;
   };
-  
+
+  this.reset = function () {
+    this.y = this.yi;
+    this.index = RandomIndex();
+    this.x = obstaclePosition[this.index];
+  };
 }
 
 function car() {
-  this.x = 385;
-  this.y = 490;
-  //this.color = c;
+  this.y = 485;
+  this.index = 1;
+  this.x = obstaclePosition[this.index];
   this.img = document.createElement("img");
   this.img.src = "images/mycar.png";
   this.carUpdate = function () {
@@ -26,15 +43,26 @@ function car() {
     ctx.drawImage(this.img, this.x, this.y, 60, 95);
   };
   this.moveLeft = function () {
-    this.x = this.x - 165;
+    if (this.index > 0) {
+      this.index--;
+      this.x = obstaclePosition[this.index];
+    }
+  };
+  this.moveRight = function () {
+    if (this.index < obstaclePosition.length - 1) {
+      this.index++;
+      this.x = obstaclePosition[this.index];
+    }
   };
 }
 
 function background(speed) {
   this.y = 0;
   this.height = animationArea.canvas.height;
-  this.max = 0 + this.height;
-  this.step = -(speed + 1);
+  this.max = this.height;
+  this.speed = speed;
+  this.speed = this.speed + 0.1;
+  this.step = -(this.speed + 1);
   this.img = document.createElement("img");
   this.img.src = "images/lane.png";
   this.print = function () {
@@ -42,9 +70,55 @@ function background(speed) {
     ctx.drawImage(this.img, 0, this.y, 500, 600);
     ctx.drawImage(this.img, 0, this.y - this.height, 500, 600);
     this.y -= this.step;
-    // console.log(this.y);
     if (this.y > this.max) {
       this.y = 0;
     }
+  };
+}
+function gameOver() {
+  if (score > highscore) {
+    localStorage.setItem("highscore", score);
+  }
+
+  clearInterval(animationArea.interval);
+  animationArea.container.position = "relative";
+  const popUp = document.createElement("div");
+  popUp.id = "popUp";
+  popUp.style.width = "30%";
+  popUp.style.height = "300px";
+  popUp.style.position = "absolute";
+  popUp.style.top = "100px";
+  popUp.style.marginLeft = "35%";
+  popUp.style.backgroundColor = `rgba(255, 255, 255, 0.2)`;
+  popUp.style.borderRadius = "15px";
+  animationArea.container.append(popUp);
+  const reLoad = document.createElement("div");
+
+  reLoad.style.position = "absolute";
+  reLoad.style.height = "50px";
+  reLoad.style.width = "100px";
+  reLoad.style.backgroundColor = "red";
+  reLoad.style.bottom = "0px";
+  reLoad.style.marginLeft = "40%";
+  reLoad.style.marginBottom = "30px";
+  reLoad.style.borderRadius = "5px";
+  reLoad.style.cursor = "pointer";
+  popUp.append(reLoad);
+  const h1  = document.createElement("h1");
+  h1.id = "gameOver";
+  h1.innerText = "Game Over !!"
+  h1.style.textAlign = "center";
+  h1.style.fontSize = "60px";
+  h1.style.marginTop = "50px";
+  h1.style.fontWeight = "600";
+  h1.style.color = "red";
+  popUp.append(h1);
+  const h3 = document.createElement("h3");
+  h3.id = "reloadTxt";
+  h3.innerText = "Play Again";
+
+  reLoad.append(h3);
+  reLoad.onclick = function () {
+    window.location.reload();
   };
 }
--- Car-Lane-Game/src/index.js
+++ Car-Lane-Game/src/index.js
@@ -1,28 +1,33 @@
 let myCar;
 let laneBackground;
 let myObstacle = [];
-let speed = 5;
+let speed = 2;
 let obstaclePosition = [60, 220, 385];
 let score = 0;
-let index = 2;
+let highscore = 0;
 
 function startAnimation() {
   animationArea.start();
   myCar = new car();
   laneBackground = new background(speed);
-  setInterval(() => {
-    let randomIndex = Math.floor(Math.random() * obstaclePosition.length);
-    myObstacle.push(new obstacle(speed, obstaclePosition[randomIndex]));
-  }, 2000);
+  myObstacle.push(new obstacle(speed, -300));
+  myObstacle.push(new obstacle(speed, -100));
+  // myObstacle.push(new obstacle(speed, -500));
+  if (localStorage.getItem("highscore") !== null) {
+    highscore = localStorage.getItem("highscore");
+  }
 }
 
-var animationArea = {
+let animationArea = {
   container: document.getElementById("canvas-container"),
   canvas: document.createElement("canvas"),
   start: function () {
-    this.canvas.width = 700;
+    this.container.style.backgroundImage = "url('images/city.jpeg')";
+    this.container.style.backgroundSize = "cover";
+    this.container.style.height = window.screen.height + "px";
+    this.canvas.width = 500;
     this.canvas.height = 600;
-    this.canvas.addEventListener("keydown", handleClick);
+    document.body.addEventListener("keydown", handleClick);
     this.context = this.canvas.getContext("2d");
     this.container.append(this.canvas);
     this.interval = setInterval(updateAnimationArea, 20);
@@ -41,30 +46,36 @@ function updateAnimationArea() {
     obstacle.obstacleUpdate();
     collisionDetect(obstacle);
   });
-  scoreCalc();
-  scoreDisplay();
+  scoreDisplay(380, 40);
+  hScoreDisplay(30, 40);
+  //console.log(myObstacle);
 }
 
 function handleClick(event) {
-  if (event.key === "37") {
+  if (event.keyCode == "37") {
     myCar.moveLeft();
-    console.log("moved left");
+  } else if (event.keyCode == "39") {
+    myCar.moveRight();
   }
 }
 
 //score calculator
 function scoreCalc() {
   setTimeout(function () {}, 2000);
-  for (let i = 0; i < myCar.length; i++) {
-    if (myCar[i].y > animationArea.canvas.height + 2) {
+  for (let i = 0; i < myObstacle.length; i++) {
+    if (myObstacle[i].y > animationArea.canvas.height + 2) {
       score++;
     }
   }
 }
 //display score
-function scoreDisplay() {
-  animationArea.context.font = "50px Comic Sans MS";
-  animationArea.context.fillText(score, 570, 100);
+function scoreDisplay(x, y) {
+  animationArea.context.font = "25px Comic Sans MS";
+  animationArea.context.fillText("Score: " + score, x, y);
+}
+function hScoreDisplay(x, y) {
+  animationArea.context.font = "25px Comic Sans MS";
+  animationArea.context.fillText("High Score: " + highscore, x, y);
 }
 
 function collisionDetect(obstacle) {
@@ -78,33 +89,4 @@ function collisionDetect(obstacle) {
   ) {
     gameOver();
   }
-}
-function gameOver() {
-  clearInterval(animationArea.interval);
-  animationArea.container.position = "relative";
-  const popUp = document.createElement("div");
-  popUp.id = "popUp";
-  popUp.style.width = "50%";
-  popUp.style.height = "300px";
-  // popUp.style.backgroundColor = "yellow";
-  popUp.style.position = "absolute";
-  popUp.style.top = "100px";
-  popUp.style.marginLeft = "24.5%";
-  popUp.style.backgroundImage = "url('images/over.png')";
-  popUp.style.backgroundSize = "cover";
-  popUp.style.backgroundPosition = "center";
-  animationArea.container.append(popUp);
-  const reLoad = document.createElement("div");
-  reLoad.style.position = "absolute";
-  reLoad.style.height = "50px";
-  reLoad.style.width = "100px";
-  reLoad.style.backgroundColor = "red";
-  reLoad.style.bottom = "0px";
-  reLoad.style.marginLeft = "300px";
-  reLoad.style.marginBottom = "30px";
-  reLoad.innerText = "Reload";
-  popUp.append(reLoad);
-  reLoad.onclick = function () {
-    window.location.reload();
-  };
 }
